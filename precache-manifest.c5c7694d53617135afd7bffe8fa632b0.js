8668359e28d188d4622033cf11ad631c0122fac0
Abhay Pratap Singh <abhaypratap3536@gmail.com>
2020-05-25 21:14:06 +0100

    Updates

---
+++ precache-manifest.c5c7694d53617135afd7bffe8fa632b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2348fe7c717d7aa8c165f7b99b774296",
+    "url": "/movie-react/index.html"
+  },
+  {
+    "revision": "6b95e334e2ecaabd81c9",
+    "url": "/movie-react/static/js/2.cc244dac.chunk.js"
+  },
+  {
+    "revision": "3c08c4f9cd51d053d502",
+    "url": "/movie-react/static/js/main.01f300c4.chunk.js"
+  },
+  {
+    "revision": "5cf0f64c94dbf5db2fd7",
+    "url": "/movie-react/static/js/runtime~main.428f8c03.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/movie-react/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/movie-react/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/movie-react/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
